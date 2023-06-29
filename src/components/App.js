acef0423d94cf70b9290733275c75e087a45eccc
hemant0933 <116268777+hemant0933@users.noreply.github.com>
2023-06-29 22:36:06 +0530

    submitting code-Thu Jun 29 2023 22:36:05 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,51 @@
 
-import React from "react";
+import React, {useState} from "react";
 import './../styles/App.css';
+import Select from 'react-select';
 
 const App = () => {
+  const [selectedOption, setSelectedOption] = useState(null);
+  const options = [
+    { value: "Delhi", label: "Delhi" },
+    { value: "Mumbai", label: "Mumbai" },
+    { value: "Bangalore", label: "Bangalore" },
+    { value: "Chennai", label: "Chennai" },
+    { value: "Kolkata", label: "Kolkata" },
+    { value: "Hyderabad", label: "Hyderabad" },
+    { value: "Pune", label: "Pune" },
+    { value: "Ahmedabad", label: "Ahmedabad" },
+    { value: "Jaipur", label: "Jaipur" },
+    { value: "Lucknow", label: "Lucknow" },
+    { value: "Kochi", label: "Kochi" },
+    { value: "Surat", label: "Surat" },
+    { value: "Kanpur", label: "Kanpur" },
+    { value: "Goa", label: "Goa" },
+    { value: "Ajmer", label: "Ajmer" },
+    { value: "Hyderabad", label: "Hyderabad" },
+    { value: "Jammu", label: "Jammu" },
+    { value: "Navi Mumbai", label: "Navi Mumbai" },
+    { value: "Jabalpur", label: "Jabalpur" },
+    { value: "Vijayawada", label: "Vijayawada" },
+    { value: "Jalander", label: "Jalander" },
+  ];
+  
+  const handleSelectOption = (option) => {
+    setSelectedOption(option)
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Autocomplete Form</h1>
+        <form>
+          <label htmlFor="city">Search cities of india: </label><br/>
+         <Select
+          options={options}
+          value={selectedOption}
+          onChange={handleSelectOption}
+          // isClearable
+          // isSearchable
+         />
+
+        </form>
     </div>
   )
 }
